6eb605d38a4ca74a44c34bf49d2b90cfa012c50a
Nikita Avdosev <avdosev1@yandex.ru>
2025-09-25 22:31:32 +0300

    simple refactoring

--- run.py
+++ run.py
@@ -1,19 +1,8 @@
+import os
+
 from PIL import Image, ImageDraw, ImageFont
-import textwrap, os
 from utils import deep_merge, read_json
 
-global_config = read_json('cards/global.json')
-concrete_config = read_json('cards/1.json')
-
-config = deep_merge(global_config, concrete_config)
-
-background_path = config['layout']
-
-# Open image
-img = Image.open(background_path).convert("RGBA")
-W, H = img.size
-
-draw = ImageDraw.Draw(img)
 
 def draw_text(text, position, font_name=None, font_size=None, fill=(0, 0, 0, 255)):
     # position: (x, y) или (x, y, w, h) — лишнее игнорим
@@ -33,30 +22,6 @@ def draw_text(text, position, font_name=None, font_size=None, fill=(0, 0, 0, 255
     draw.text((x, y), text, font=font, fill=fill)
     return {"pos": (x, y), "font_size": getattr(font, "size", None)}
 
-
-draw_text(
-    text=config['username_info']['content'],
-    position=config['username_info']["position"],
-    font_name=config['username_info']['font'],
-    font_size=45,
-)
-
-draw_text(
-    text=config['cardname_info']['content'],
-    position=config['cardname_info']["position"],
-    font_name=config['cardname_info']['font'],
-    font_size=60,
-)
-
-# сохраняем итог в файл
-out_path = "tmp/card.png"
-os.makedirs(os.path.dirname(out_path), exist_ok=True)
-img.save(out_path)
-
-print("saved tmp image:", out_path)
-
-from PIL import Image
-import os
 
 def images_to_pdf(image1_path, image2_path, output_pdf_path):
     """
@@ -147,6 +112,41 @@ def resize_image_to_exact(img, target_width, target_height):
 
 # Пример использования
 if __name__ == "__main__":
+
+    global_config = read_json('cards/global.json')
+    concrete_config = read_json('cards/1.json')
+
+    config = deep_merge(global_config, concrete_config)
+
+    background_path = config['layout']
+
+    # Open image
+    img = Image.open(background_path).convert("RGBA")
+    W, H = img.size
+
+    draw = ImageDraw.Draw(img)
+
+    draw_text(
+        text=config['username_info']['content'],
+        position=config['username_info']["position"],
+        font_name=config['username_info']['font'],
+        font_size=45,
+    )
+
+    draw_text(
+        text=config['cardname_info']['content'],
+        position=config['cardname_info']["position"],
+        font_name=config['cardname_info']['font'],
+        font_size=60,
+    )
+    
+    # сохраняем итог в файл
+    out_path = "tmp/card.png"
+    os.makedirs(os.path.dirname(out_path), exist_ok=True)
+    img.save(out_path)
+
+    print("saved tmp image:", out_path)
+
     # Укажите пути к вашим изображениям
     photo_path = out_path
     layout_path = out_path
